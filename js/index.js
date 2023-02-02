de7cab1bb8bc29c2efe9d519661c705b224781cf
Maksim <dolya.m.mos.av@gmail.com>
2023-02-02 23:14:45 +0300

    apdate project

---
+++ js/index.js
@@ -0,0 +1,49 @@
+// const popupTrigger = document.querySelector(".profile__edit-button"),
+//   popup = document.querySelector(".popup"),
+//   popupCloseBtn = document.querySelector(".popup__close"),
+//   popupInputName = document.querySelector(".popup__input-name"),
+//   popupInputAbout = document.querySelector(".popup__input-about"),
+//   formElement = document.querySelector(".popup__form"),
+//   profileTitle = document.querySelector(".profile__title"),
+//   profileDescription = document.querySelector(".profile__description"),
+//   saveBtn = document.querySelector(".popup__save-button");
+
+//  const saveBtnCard = document.querySelector(".popup__save-btn_add_card");
+
+// const formAddNewCard = cardsPopup.querySelector(".popup__form");
+// const titleInput = document.querySelector(".popup__input-title");
+// const linkInput = document.querySelector(".popup__input-link");
+
+// const templateList = document.querySelector(".elements__list");
+// const templateElement = document.querySelector(".template").content;
+
+// infoEdit.addEventListener("click", function () {
+//   openPopup(infoPopup);
+// });
+// infoPopupClose.addEventListener("click", function () {
+//   closePopup(infoPopup);
+// });
+
+// function openPopup(popup) {
+//   popup.classList.add("popup_opened");
+// }
+
+// function closePopup(popup) {
+//   popup.classList.remove("popup_opened");
+// }
+
+// cardPopupOpen.addEventListener("click", function () {
+//   openPopup(cardsPopup);
+// });
+// cardPopupClose.addEventListener("click", function () {
+//   closePopup(cardsPopup);
+// });
+
+// function formSubmitHandler(e) {
+//   e.preventDefault();
+//   infoName.textContent = nameInput.value;
+//   infoDescription.textContent = jobInput.value;
+
+//   closePopup(infoPopup);
+// }
+// formElement.addEventListener("submit", formSubmitHandler);
